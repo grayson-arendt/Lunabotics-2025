4c94f15705464b63ce5cd2fd26687c71ee69eb27
Grayson Arendt <97147107+grayson-arendt@users.noreply.github.com>
2024-09-18 17:14:44 -0500

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -19,7 +19,6 @@ install_ros_packages() {
 
 # Install miscellaneous packages
 install_misc_packages() {
-    sudo apt install git cmake
     sudo add-apt-repository ppa:graysonarendt/sparkcan
     sudo apt update
     sudo apt install sparkcan
